5e22396b704454f2a2ce1c7abb65505fc928b8cb
Jason <itchison@gmail.com>
2020-05-15 11:00:01 -0700

    initial

---
+++ FlaskAPI/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import os
+import markovify
+
+app = Flask(__name__)
+
+
+# Get raw text as string.
+with open("catfacts/catfacts.txt") as f:
+    text = f.read()
+
+# Build the model.
+text_model = markovify.Text(text)
+
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
